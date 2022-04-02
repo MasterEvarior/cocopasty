fb4bb4679ac9c2c61ddb70604b7800019cd63a42
MasterEvarior <masterevarior@protonmail.com>
2022-04-02 22:18:54 +0200

    Added code editor

--- cocopasty-frontend/src/App.vue
+++ cocopasty-frontend/src/App.vue
@@ -1,12 +1,70 @@
 <template>
-  <p>Cocopasty</p>
+  <div class="container">
+    <h1>Cocopasty</h1>
+    <CodeEditor
+      :languages="languages"
+      :language_selector="true"
+      :autofocus="true"
+      width="100%"
+      v-model="code"/>
+  </div>
 </template>
 
 <script>
+import CodeEditor from 'simple-code-editor';
 
 export default {
   name: 'App',
   components: {
+    CodeEditor
+  },
+  data() {
+    return {
+      code: '',
+      languages: [
+        ['javascript', 'JS'],
+        ['python', 'Python'],
+        ['brainfuck','Brainfuck'],
+        ['applescript','AppleScript'],
+        ['xml', 'XML'],
+        ['bash','Bash'],
+        ['basic','Basic'],
+        ['c','C'],
+        ['clojure','Clojure'],
+        ['coffeescript','CoffeeScript'],
+        ['csharp','C#'],
+        ['css','CSS'],
+        ['markdown','Markdown'],
+        ['dart','Dart'],
+        ['django','Django'],
+        ['dockerfile','Dockerfile'],
+        ['ruby','Ruby'],
+        ['fortran','Fortran'],
+        ['go','Go'],
+        ['haskell','Haskell'],
+        ['java','Java'],
+        ['javascript','JavaScript'],
+        ['json','JSON'],
+        ['kotlin','Kotlin'],
+        ['lisp','Lisp'],
+        ['lua','Lua'],
+        ['perl','Perl'],
+        ['powershell','PowerShell'],
+        ['python','Python'],
+        ['r','R'],
+        ['rust','Rust'],
+        ['scala','Scala'],
+        ['scilab','Scilab'],
+        ['scss','SCSS'],
+        ['shell','Shell'],
+        ['sql','SQL'],
+        ['swift','Swift'],
+        ['yaml','YAML'],
+        ['typescript','TypeScript'],
+        ['vbscript','VBScript'],
+        ['vim','Vim']
+      ]
+    }
   }
 }
 </script>
@@ -19,5 +77,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  margin: 0 auto;
+  max-width: 580px; 
 }
 </style>
