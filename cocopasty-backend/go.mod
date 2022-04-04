910f77ff84ef1aaba8e118f654b1260b9a7db8e1
MasterEvarior <masterevarior@protonmail.com>
2022-04-04 12:38:11 +0200

    Added Docker to backend + implemented small test API

--- cocopasty-backend/go.mod
+++ cocopasty-backend/go.mod
@@ -1,3 +1,9 @@
 module cocopasty-backend
 
 go 1.18
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+)
--- cocopasty-backend/main.go
+++ cocopasty-backend/main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
 )
 
+type CodeSnippet struct {
+	Code     string `json:Code`
+	Language string `json:Language`
+}
+
 func main() {
-	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/", handleRequests)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping(ctx).Result()
+	fmt.Println(pong, err)
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "version 1")
+func handleRequests(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		snippet := CodeSnippet{"Console.log(12)", "js"}
+		json.NewEncoder(w).Encode(snippet)
+	case "POST":
+		//TODO
+	default:
+		io.WriteString(w, "Sorry, only GET and POST methods are supported.")
+	}
 }
