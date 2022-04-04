dbef4b287d9d314930ede661aeaf115e279047db
MasterEvarior <masterevarior@protonmail.com>
2022-04-04 13:30:09 +0200

    Restructured API and fixed CORS issue

--- cocopasty-backend/go.mod
+++ cocopasty-backend/go.mod
@@ -3,7 +3,6 @@ module cocopasty-backend
 go 1.18
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/rs/cors v1.8.2 // indirect
 )
--- cocopasty-backend/main.go
+++ cocopasty-backend/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 
-	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 type CodeSnippet struct {
@@ -17,33 +15,29 @@ type CodeSnippet struct {
 }
 
 func main() {
-	http.HandleFunc("/", handleRequests)
+	//Initialize router
+	router := mux.NewRouter()
 
-	err := http.ListenAndServe(":8080", nil)
+	router.HandleFunc("/", handleGets).Methods("GET")
+	router.HandleFunc("/", handlePosts).Methods("POST")
+
+	corsHandler := cors.Default().Handler(router)
+
+	//Start server
+	err := http.ListenAndServe(":8080", corsHandler)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	ctx := context.Background()
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
-
-	pong, err := client.Ping(ctx).Result()
-	fmt.Println(pong, err)
 }
 
-func handleRequests(w http.ResponseWriter, r *http.Request) {
+func handlePosts(w http.ResponseWriter, r *http.Request) {
 
-	switch r.Method {
-	case "GET":
-		snippet := CodeSnippet{"Console.log(12)", "js"}
-		json.NewEncoder(w).Encode(snippet)
-	case "POST":
-		//TODO
-	default:
-		io.WriteString(w, "Sorry, only GET and POST methods are supported.")
-	}
+}
+
+func handleGets(w http.ResponseWriter, r *http.Request) {
+	snippet := CodeSnippet{"Console.log(12)", "js"}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(snippet)
 }
