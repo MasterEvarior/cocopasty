712833c18a50f887f04d277384f2abe220a35d3b
MasterEvarior <contaxt@giannin.dev>
2023-01-06 13:39:13 +0100

    Replaced non-functioning font dependency

--- cocopasty-frontend/src/main.js
+++ cocopasty-frontend/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import Toaster from '@meforma/vue-toaster';
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
 import App from './App.vue'
 
 let app = createApp(App)
--- cocopasty-frontend/vue.config.js
+++ cocopasty-frontend/vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin")
 
 // vue.config.js
 module.exports = {
@@ -8,10 +7,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
-        new GoogleFontsPlugin({
-            fonts: [
-                { family: "Roboto Mono", variants: [ "500", "700" ] }
-            ]
-        })
+        
     ]}
 })
